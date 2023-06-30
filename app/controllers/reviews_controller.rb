1a0c0e6f037b85118ff90f4d5dad4d7d30c714d0
KiruiTito <titokirui402@gmail.com>
2023-06-30 10:49:33 +0300

    Completed assignment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,7 @@
+# app/controllers/reviews_controller.rb
 class ReviewsController < ApplicationController
-
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
   end
-
 end
